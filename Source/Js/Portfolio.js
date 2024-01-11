c3d381f33fd9e3283c488ec2494ebbc1b744191e
Niranjan <niranjan.jambhulkar@outlook.com>
2024-01-11 21:28:03 +0530

    Update

--- Source/Js/Portfolio.js
+++ Source/Js/Portfolio.js
@@ -5,3 +5,37 @@ function work(){
 function cv(){
     location.href = "https://drive.google.com/file/d/1NvKiVPMwsVfBHM3S4tZGrYyXLEHf6TbI/view?usp=sharing";
 }
+
+function mover1(){
+    document.getElementById("sub1").innerHTML = '<p style="background-image=url(.)">Employee Management System</p>'
+}
+
+function mout1(){
+    document.getElementById("sub1").innerHTML = ""
+}
+
+function mover2(){
+    document.getElementById("sub2").innerHTML = "Calculator"
+}
+function mout2(){
+    document.getElementById("sub2").innerHTML = ""
+}
+
+function mover3(){
+    document.getElementById("sub3").innerHTML = "Portfolio"
+}
+function mout3(){
+    document.getElementById("sub3").innerHTML = ""
+}
+
+function ems(){
+    location.href = "https://github.com/Niranjan-Jambhulkar/Employee-Management-System"
+}
+
+function cal(){
+    location.href = "https://github.com/Niranjan-Jambhulkar/Calculator"
+}
+
+function port(){
+    location.href = "https://github.com/Niranjan-Jambhulkar/Portfolio"
+}
