0550cbf6e692ef446378eb2a472adf3803c41507
Niranjan <niranjan.jambhulkar@outlook.com>
2024-01-11 14:14:01 +0530

    complete structure created

--- Source/Js/Portfolio.js
+++ Source/Js/Portfolio.js
@@ -1,3 +1,7 @@
 function work(){
     location.href = "https://github.com/Niranjan-Jambhulkar?tab=repositories";
 }
+
+function cv(){
+    location.href = "https://drive.google.com/file/d/1NvKiVPMwsVfBHM3S4tZGrYyXLEHf6TbI/view?usp=sharing";
+}
