c9a5cb17d0b48f411d9f17fcc3b511db04b1b35e
Niranjan <niranjan.jambhulkar@outlook.com>
2024-01-19 21:43:15 +0530

    update

--- Source/Js/Portfolio.js
+++ Source/Js/Portfolio.js
@@ -6,6 +6,22 @@ function cv(){
     location.href = "https://drive.google.com/file/d/1NvKiVPMwsVfBHM3S4tZGrYyXLEHf6TbI/view?usp=sharing";
 }
 
+function ems(){
+    location.href = "https://github.com/Niranjan-Jambhulkar/Employee-Management-System"
+}
+
+function cal(){
+    location.href = "https://github.com/Niranjan-Jambhulkar/Calculator"
+}
+
+function por(){
+    location.href = "https://github.com/Niranjan-Jambhulkar/Portfolio"
+}
+
+function hire(){
+    location.href = "mailto:niranjan.jambhulkar@outlook.com"
+}
+
 function mover1(){
     document.getElementById("sub1").innerHTML = '<p style="background-image=url(.)">Employee Management System</p>'
 }
@@ -26,20 +42,4 @@ function mover3(){
 }
 function mout3(){
     document.getElementById("sub3").innerHTML = ""
-}
-
-function ems(){
-    location.href = "https://github.com/Niranjan-Jambhulkar/Employee-Management-System"
-}
-
-function cal(){
-    location.href = "https://github.com/Niranjan-Jambhulkar/Calculator"
-}
-
-function por(){
-    location.href = "https://github.com/Niranjan-Jambhulkar/Portfolio"
-}
-
-function hire(){
-    location.href = "mailto:niranjan.jambhulkar@outlook.com"
 }
